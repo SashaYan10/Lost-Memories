c09b8dac9b2ddafae6d2752a4d71fb59d54d14e1
SashaYan10 <sashayanus10@gmail.com>
2025-06-02 13:58:23 +0300

    Some Dialogue Fixes

--- Assets/Scripts/Action Objects/ActDialogue.cs
+++ Assets/Scripts/Action Objects/ActDialogue.cs
@@ -6,9 +6,23 @@ public class ActDialogue : MonoBehaviour
 {
     [SerializeField] private GameObject dialogue;
 
+    private bool recentlyActivated = false;
+    private float cooldownTime = 1.5f;
+
     public void ActiveDialogue()
     {
-        dialogue.SetActive(true);
+        if (!recentlyActivated && !DialogueSystem.DialogueHolder.IsDialogueActive)
+        {
+            dialogue.SetActive(true);
+            StartCoroutine(DialogueCooldown());
+        }
+    }
+
+    private IEnumerator DialogueCooldown()
+    {
+        recentlyActivated = true;
+        yield return new WaitForSeconds(cooldownTime);
+        recentlyActivated = false;
     }
 
     public bool DialogueActive()
